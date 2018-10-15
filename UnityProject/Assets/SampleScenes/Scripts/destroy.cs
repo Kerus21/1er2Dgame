88d652fe89cebc21a8304bf237f83e03ebd01590
Kerus21 <johnnymigo@gmail.com>
2018-10-15 18:54:08 +0200

    Juego 2D con menus

---
+++ UnityProject/Assets/SampleScenes/Scripts/destroy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class destroy : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag=="Player")
+        {
+            SceneManager.LoadScene(2);
+        }
+        else
+        {
+            if (collision.gameObject.transform.parent)
+            {
+                Destroy(collision.gameObject.gameObject.transform.parent);
+            }
+            else
+            {
+                Destroy(collision.gameObject);
+            }
+            
+        }
+    }
+    public void jugar()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void noReiniciar()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ UnityProject/Assets/SampleScenes/Scripts/instanciador.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instanciador : MonoBehaviour {
+    public GameObject[] objetos;
+    public float min = 1f, max = 2f; 
+
+	// Use this for initialization
+	void Start () {
+        instanciar();
+	}
+	void instanciar()
+    {
+        Instantiate(objetos[Random.Range(0, objetos.Length)], transform.position, Quaternion.identity);
+        Invoke("instanciar", Random.Range(min, max));
+    }
+}
